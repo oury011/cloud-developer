ea67b4b015b377fbed24f8622927f11cc8f7c75f
Diallo <dialloamadou10@yahoo.com>
2022-09-07 17:56:43 +0000

    adding filtering api rest

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+//import express from 'express';
+import express, { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -30,6 +31,21 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
+
+  app.get( "/filteredimage", 
+  async ( req: Request, res: Response ) => {
+    const { image_url } = req.query as {image_url:string};
+
+    if ( !image_url ) {
+      return res.status(400).send(`image_url is required`);
+    }
+
+    const filteredPath: string = await filterImageFromURL(image_url);
+
+    return res.status(200).sendFile(filteredPath,()=>{
+      deleteLocalFiles([filteredPath])
+    });
+});
   
   // Root Endpoint
   // Displays a simple message to the user
