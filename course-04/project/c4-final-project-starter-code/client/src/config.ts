552eb933fa709da6b368acba395f34333fb01ec1
Diallo <dialloamadou10@yahoo.com>
2022-10-08 02:14:37 +0000

    deploy serverless app

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'aiht1a8mr6'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map. For example:
+  // Fait: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-zwb9t88n.us.auth0.com',            // Auth0 domain
+  clientId: '9h5JsVMR6q0yTZkQiiacYNFtc6HFxxba',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
